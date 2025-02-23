04d995ebc8198ec7fe75717115d0324f21ec8812
pavan thomas <pavansunnythomas@gmail.com>
2025-02-23 22:50:01 +0530

    configurations added

--- src/main/java/com/pavan/todoapp/controller/AppController.java
+++ src/main/java/com/pavan/todoapp/controller/AppController.java
@@ -16,6 +16,7 @@ public class AppController {
         return Arrays.asList(
             new Task(1, "buy Milk", "Buy 2 amul milk 1 litre pack"),
             new Task(2, "buy Eggs", "Buy 10 whole eggs and do not break them")
+            // new Task(3, "buy Cream", "Buy whip cream for making icing")
             );
     }
 }
