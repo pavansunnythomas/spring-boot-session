2fa32fdba88b5a927dd451a4b4ab7ca897c9e7fc
pavan thomas <pavansunnythomas@gmail.com>
2025-02-23 23:01:17 +0530

    corrected extra class

--- src/main/java/com/pavan/todoapp/controller/AppController.java
+++ src/main/java/com/pavan/todoapp/controller/AppController.java
@@ -1,21 +1,15 @@
 package com.pavan.todoapp.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.pavan.todoapp.model.Task;
 
 @RestController
 public class AppController {
 
-    @GetMapping("get-tasks")
-    public List<Task> getTasks() {
-        return Arrays.asList(
-            new Task(1, "buy Milk", "Buy 2 amul milk 1 litre pack"),
-            new Task(2, "buy Eggs", "Buy 10 whole eggs and do not break them")
-            );
+
+    @GetMapping("hello")
+    public String helloWorld() {
+        return "Hello World";
     }
+
 }
